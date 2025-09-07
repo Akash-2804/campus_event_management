d994516fd44ba98d7b5cde0fa43cec2e34c10d3e
Akash-2804 <akashparuchuri28@gmail.com>
2025-09-07 14:55:57 +0530

    Add files via upload

---
+++ seed_data.py
@@ -0,0 +1,15 @@
+from app import db, app
+from app.models import College, Student
+
+with app.app_context():
+    
+    college = College(name="REVA University")
+    db.session.add(college)
+    db.session.commit()
+
+    
+    student = Student(name="akash", college_id=college.id)
+    db.session.add(student)
+    db.session.commit()
+
+    print(f"✅ College ID: {college.id}, Student ID: {student.id}")
